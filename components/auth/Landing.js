5e3a3513630f54a90012a5979f618cc2b1147f64
leeseongyub <48345758+leeseongyub@users.noreply.github.com>
2021-11-27 18:26:18 +0900

    Navigation 초기설정

---
+++ components/auth/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Landing = () => {
+  return (
+    <View>
+      <Text>landing</Text>
+    </View>
+  );
+};
+
+export default Landing;

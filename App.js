3dc215baa27efebb1bead63bea5a9872d927e1f8
leeseongyub <48345758+leeseongyub@users.noreply.github.com>
2021-11-27 18:50:32 +0900

    SafeAreaView 설정

--- App.js
+++ App.js
@@ -2,21 +2,31 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
 import Landing from "./components/auth/Landing";
+import Resigster from "./components/auth/Resigster";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen
-          name="Landing"
-          component={Landing}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Register">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Resigster}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
---
+++ components/auth/Resigster.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Resigster = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>Resigster</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Resigster;
